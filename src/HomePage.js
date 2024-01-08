f04aefbe361c3d21a952e2cc697c378434465b25
Diwas15 <89520486+Diwas15@users.noreply.github.com>
2024-01-08 23:27:38 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,22 +1,26 @@
-import React,{useState,useEffect} from "react"
+import React,{useState,useEffect,createContext} from "react"
 import logo from './logo.png'
 import SignOut from "./SignOut";
 import './HomePage.css'
 import { useLocation } from "react-router-dom";
 import SavedNote from "./SavedNote";
 
+const MyContext = createContext({
+    notes:[],
+    setNotes:(data)=>{}
+})
 
-function HomePage(props) {
+function HomePage() {
     const location = useLocation();
-    //console.log(location.state.email);
-    const user_email=location.state.email;
+    console.log("this is homepage "+location.state.email);
+    const [user_email,setEmail]=useState(location.state.email);
     //console.log(user_email);
     const [formValue,setFormValue]=useState();
     const [notes,setNotes]=useState([]);
     
     useEffect(()=>{
-        console.log(user_email);
-        fetch('https://note-app-test-pe9o.onrender.com/notes/new',{
+        console.log("your mail:"+ user_email);
+        fetch('http://localhost:9000/notes/new',{
             method:'POST', 
             headers:{
                 "Content-Type":"application/json",
@@ -28,9 +32,8 @@ function HomePage(props) {
             //console.log(data);
         })
         .catch((e)=>console.log(e));
-        //console.log(notes)
-        setFormValue('');
-
+        console.log(notes)
+        
         
        
     },[])
@@ -40,8 +43,8 @@ function HomePage(props) {
             user_email:user_email,
             body:formValue
         };
-        console.log(formValue);
-        fetch('https://note-app-test-pe9o.onrender.com/notes/add',{
+        //console.log(formValue);
+        fetch('http://localhost:9000/notes/add',{
             method:'POST',
             headers:{
                 "Content-Type":"application/json",
@@ -49,36 +52,45 @@ function HomePage(props) {
             },
             body:JSON.stringify(data)
         }).then((res)=>{
-            console.log(res);
+            //console.log(res);
+           setNotes(oldNotes=>[...oldNotes,data]);
         }).catch((err)=>{
             console.log(err);
         })
+        setFormValue('');
+
     }
     return (
-      <>
-          <div className="header">
-            <div className="logo">
-                <img src={logo} alt="not found" />
-            </div>
-            <div className="title">Take Your Notes!!</div>
-            <SignOut/>
-          </div>
-          <div className="content">
-            <div className="user_notes">
-               {notes.map((note)=>{
-                return <SavedNote key={note.id} body={note.body}/>
-               })}
-            </div>
-            <div className="notes_adder">
-                <form>
-                    <textarea name="notes" value={formValue} id="_text_" cols="60" rows="20" placeholder="write your notes" onChange={(e)=>setFormValue(e.target.value)}></textarea>
-                    <div className="form_btn"><button id="f_btn" disabled={!formValue} onClick={(e)=>{handleAdd(e)}}>Add</button></div>
-                </form>
-            </div>
-          </div>
-          <div className="footer"></div>
+       <>
+        <MyContext.Provider value={{notes,setNotes}}>
+            
+                <div className="header">
+                    <div className="logo">
+                        <img src={logo} alt="not found" />
+                    </div>
+                    <div className="title">Take Your Notes!!</div>
+                    <SignOut/>
+                </div>
+                <div className="content">
+                    <div className="user_notes">
+                    {notes.map((note,id)=>{
+                        return <SavedNote key={id} __id={note._id} body={note.body} email={user_email}/>
+                        
+                    })}
+                    </div>
+                    <div className="notes_adder">
+                        <form  onSubmit={(e)=>{e.target.reset();handleAdd(e)}}>
+                            <textarea className="notes" name="notes" cols="70" rows="20" placeholder="write your notes" onChange={(e)=>setFormValue(e.target.value)}></textarea>
+                            <div className="form_btn"><button type="submit" id="f_btn">Add</button></div>
+                        </form>
+                    </div>
+                </div>
+                <div className="footer"></div>
+           
+        </MyContext.Provider>
       </>
     )
   }
 
   export default HomePage;
+  export {MyContext};
